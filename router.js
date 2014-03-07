64e101e9d6aeb22aea0b85ab08614d43a019ccea
ever1022 <ever1022@gmail.com>
2014-03-07 13:06:57 +0800

    Catch exception

--- router.js
+++ router.js
@@ -4,20 +4,24 @@ var requestHandlers = require("./requestHandlers");
 function route(pathName, response) {
     console.log("About to route a request for " + pathName);
     
-    if(pathName !== "/" && pathName.length !== 0) {
-        if(pathName.substring(0,1) === "/") {
-            pathName = pathName.substring(1, pathName.length + 1);
-        }
-        var fileStats = fs.statSync(pathName);
-        if(fileStats.isDirectory()) {
-            console.log("A directory is requested...");
-            requestHandlers.files(pathName, response);
-        } else if (fileStats.isFile()) {
-            console.log("A file is requested...");
-            requestHandlers.openFile(pathName, response);
-        }
-        return;
-    } 
+    try {
+        if(pathName !== "/" && pathName.length !== 0) {
+            if(pathName.substring(0,1) === "/") {
+                pathName = pathName.substring(1, pathName.length + 1);
+            }
+            var fileStats = fs.statSync(pathName);
+            if(fileStats.isDirectory()) {
+                console.log("A directory is requested...");
+                requestHandlers.files(pathName, response);
+            } else if (fileStats.isFile()) {
+                console.log("A file is requested...");
+                requestHandlers.openFile(pathName, response);
+            }
+            return;
+        } 
+    } catch (e) {
+        console.log("An exception was thrown " + e);
+    }
 
     console.log("Cannot handle the path!");
     response.writeHead(404, {"Content-Type": "text/plain"});
