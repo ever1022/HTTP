aa90350a0cb67269efbecd38a3c38c5986e831f3
ever1022 <ever1022@gmail.com>
2014-05-04 22:03:27 +0800

    Add chat using socket.io

--- server.js
+++ server.js
@@ -1,26 +1,60 @@
 var http = require("http");
+var connect = require("connect");
+var socketIO = require("socket.io");
 var logger = require("./logger");
 var router = require("./router");
 
 function start(route) {
     function onRequest(request, response) {
-	var postData = "";
-	request.addListener(
-	    "data",
+        var postData = "";
+        request.addListener(
+            "data",
             function(postDataChunk) {
-		postData += postDataChunk;
-	    }
-	);
+                postData += postDataChunk;
+            }
+        );
 
-	request.addListener(
-	    "end",
-	    function() {
-        	route(request, response, postData);
-	    }
-	);
+        request.addListener(
+            "end",
+            function() {
+                route(request, response, postData);
+            }
+        );
     }
-    http.createServer(onRequest).listen(8888);
+    var app = connect();
+    app.use(onRequest);
+    var httpServer = http.createServer(app).listen(8888);
     logger.i("Node.js HTTP server has started.");
+
+    var webSocket = socketIO.listen(httpServer);
+    webSocket.on(
+        "connection",
+        function(socket) {
+            logger.i("Socket is connected.");
+            socket.on(
+                'addme',
+                function(username) {
+                    socket.username = username;
+                    socket.emit('chat', 'SERVER', 'You have connected');
+                    socket.broadcast.emit('chat', 'SERVER', username + ' is on deck');
+                }
+            );
+            socket.on(
+                'sendchat',
+                function(data) { 
+                    socket.emit('chat', socket.username, data);
+                    socket.broadcast.emit('chat', socket.username, data);
+                 }
+            );
+
+            socket.on(
+                'disconnect',
+                function() {
+                    socket.emit('chat', 'SERVER', socket.username + ' has left the building');
+                }
+            );
+        }
+    );
 }
 
 exports.start = start;
